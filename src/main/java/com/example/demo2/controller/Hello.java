9554215d9e257cb57885f2d50159cb36efa54507
Winkey0428 <191win@163.com>
2021-04-11 18:37:24 +0800

    add hello controller

---
+++ src/main/java/com/example/demo2/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.example.demo2.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("world")
+    public String hello_world(){
+        return  "hello_world";
+    }
+}
